91af6957838f7640ef86b9c8132242fe42aa5036
Domi <dominikabobik12@gmail.com>
2022-01-20 22:57:30 -0500

    additions

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -15,6 +15,14 @@ const Projects: NextPage<PageProps> = () => {
             <h2>Resistor Color Decoder</h2>
             <p>Made a web app that finds color code based on the resistance of the basic circuit component.</p>
             <p>Technologies: TypeScript, React Native, Expo</p>
+            <h2>Tic Tac Toe</h2>
+            <p>Created a console-run tic tac toe game for 2 players using C++.</p>
+            <h2>Travel App</h2>
+            <p>Created a Xamarin Forms app that integrated an SQLite database, which allows users to enter the name of the city visited, country, and continent.</p>
+            <p>Technologies: C#, Xamarin Forms, SQLite</p>
+            <h2>Snowflakes</h2>
+            <p>Made a JavaFX application that produces window filled with randomly generated snowflakes.</p>
+            <img src={'/snowflake.jpg'} alt="logo" className={styles.snowflake}/>
             <h2>Calculator App</h2>
             <p>Wrote an android application in Java that performs 12 distinct mathematical operations. Designed a scalable, vertical
                 layout that is more suitable than the default one.</p>
