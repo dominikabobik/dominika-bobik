e3756e05e2a165fd542babdeadd2d8c9bf2398c7
Domi <dominikabobik12@gmail.com>
2022-05-27 00:26:35 -0400

    Stuff

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -12,7 +12,9 @@ const Experience: NextPage = () => {
 				<body className={styles.text}>
 				<h1>Professional Experience</h1>
 				<div className={styles.position}>
-					<h4>Software Engineering Intern, Open Systems International</h4>
+					<h4>Software Engineering Intern,
+						<a href={'https://www.osii.com/'} className={styles.links}> Open Systems International</a>
+					</h4>
 					<p>Developed new features for the power outage management system “Compass” for various customers
 						ranging from private firms to national electric corporations. Implemented dynamic elements in
 						Compass’s map software using the MapBox API while maintaining cross-platform compatibility and a
@@ -21,8 +23,15 @@ const Experience: NextPage = () => {
 				</div>
 				<h1>Research Experience</h1>
 				<div className={styles.position}>
-					<h4>Undergraduate Research Assistant-
-						<a href={"https://snp.cs.mtu.edu/"}>Snp Lab</a>
+					<h4>Undergraduate Research Assistant</h4>
+					<i>“Autonomy in robotics systems“</i>
+					<p>Researched concepts crucial for robotics. Learned basis of ROS navigation stack. Debugged undesired robot behavior
+						using RViz. Wrote the documentation.</p>
+					<a href={"/FinalReport.pdf"} className={styles.links} target={"_blank"} rel={"noreferrer"}>Final Report</a>
+				</div>
+				<div className={styles.position}>
+					<h4>Undergraduate Research Assistant -
+						<a href={"https://snp.cs.mtu.edu/"} className={styles.links} target={"_blank"} rel={"noreferrer"}> Security and Privacy Lab</a>
 					</h4>
 					<i>“Towards Secure Decentralized Cloud Storage by Leveraging Blockchain Technology”</i>
 					<p>Researched error-correcting codes that are suitable for flash memory in a decentralized cloud
@@ -33,37 +42,39 @@ const Experience: NextPage = () => {
 						provided by AWS and how other technologies can be adapted to IoT environments.</p>
 				</div>
 				<div className={styles.position}>
-					<h4>Undergraduate Research Assistant-
-						<a href={"https://abadi.me.mtu.edu/"}>Abadi Lab</a>
+					<h4>Undergraduate Research Assistant -
+						<a href={"https://abadi.me.mtu.edu/"} className={styles.links} target={"_blank"} rel={"noreferrer"}> Abadi Lab</a>
 					</h4>
 					<p>Conducted the study on developing fully mature cardiac cells from neonatal rat cardiomyocytes.
 						Maintained cell culture seeded on a conductive carbon nanotube scaffold (CNT), imagined cell
 						culture using CIF and SEM Microscopes, participated in CardiomyocyteIsolation Protocol, and much
 						more.</p>
 					<i>“Mechanical Stimulation of Cardiomyocytes Seeded on the Carbon Nanotube ForestScaffold for
-						Producing Mature and Functional Cells”</i>
+						Producing Mature and Functional Cells” -
+						<a href={'/URIP2020Project.pdf'} className={styles.links} target={"_blank"} rel={"noreferrer"}> Research Proposal</a>
+					</i>
 					<p>Using Siemens NX designed a custom device that provided dual electrical and mechanical stimuli to
 						the cells.</p>
+
 				</div>
 				<h1>
 					Achievements
 				</h1>
+				<a href={'/poster.jpg'} target={"_blank"} rel={"noreferrer"}>
 				<div className={styles.position}>
-					<h4>Presenter at Computing[MTU] Showcase</h4>
-					<i>Teaching Computational Thinking with Modeling and Simulations</i>
-					<img src={'/poster.jpg'} alt={'logo'} className={styles.image}/>
+						<h4>Presenter at Computing[MTU] Showcase</h4>
+						<i>Teaching Computational Thinking with Modeling and Simulations</i>
+						<img src={'/poster.jpg'} alt={'logo'} className={styles.image}/>
 				</div>
+				</a>
 				<h4 className={styles.position}>Electrical and Computer Engineering Undergraduate Advisory Board</h4>
 				<h4 className={styles.position}>{"Dean's List"}</h4>
-				<h4 className={styles.position}>{"GLIAC Freshman of the Year - Women's Tennis - 2019"}</h4>
-				<h4 className={styles.position}>{"GLIAC First Team All Conference - Women's Tennis  - 2019"}</h4>
-				<h4 className={styles.position}>{"GLIAC Second Team All Conference - Women's Tennis - 2020"}</h4>
+				<h4 className={styles.position}>{"GLIAC Freshman of the Year and First Team All Conference - Women's Tennis - 2019"}</h4>
 				<h1>
 					Part-time Employment
 				</h1>
 				<h4 className={styles.position}>Learning Center Coach - Electrical and Computer Engineering</h4>
-				<h4 className={styles.position}>Teaching Lab Assistant - Electric Circuits II</h4>
-				<h4 className={styles.position}>Teaching Lab Assistant - Introduction to Programming</h4>
+				<h4 className={styles.position}>Teaching Lab Assistant - Introduction to Programming, Electric Circuits II</h4>
 				<h4 className={styles.position}>Tutor - Mathematics, Chemistry, Physics</h4>
 				<h4 className={styles.position}>Gates Tennis Center Lobby Attendant</h4>
 				</body>
